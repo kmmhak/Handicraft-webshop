4a89c1e62b17cf9d57ae0d74292b731c136df31c
Kristiina <kristiina.hakkala@gmail.com>
2022-04-12 06:50:26 +0000

    allow users to change their name and email

--- routes/user.router.js
+++ routes/user.router.js
@@ -12,5 +12,6 @@ passport.use(jwtStrategy);
 userRouter.get("/", passportJwt(), controller.getAll);
 userRouter.get("/:id", passportJwt(), controller.getById);
 userRouter.post("/change", passportJwt(), controller.changePassword);
+userRouter.patch("/", passportJwt(), controller.updateInfo);
 
 export default userRouter;
