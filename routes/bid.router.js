f8d240e893470f9e32ed53be07ee0b5380c8062a
Kristiina <kristiina.hakkala@gmail.com>
2022-04-19 21:44:32 +0300

    add making bids

--- routes/bid.router.js
+++ routes/bid.router.js
@@ -1,8 +1,15 @@
 import { Router } from "express";
+import passport from "passport";
 import * as controller from "../controllers/bid.controller.js";
+import {
+  passportJwt,
+  jwtStrategy,
+} from "../middleware/passport-jwt.middleware.js";
 
 const bidRouter = Router();
+passport.use(jwtStrategy);
 
-bidRouter.get("/users/:id", controller.getById);
+bidRouter.get("/user/:id", controller.getById);
+bidRouter.post("/listing/:id", passportJwt(), controller.makeBid);
 
 export default bidRouter;
