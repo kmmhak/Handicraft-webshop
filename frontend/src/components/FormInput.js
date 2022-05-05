6209f03afa84af743a0009f4acf69b2a746676b6
Kristiina <kristiina.hakkala@gmail.com>
2022-05-05 16:07:18 +0300

    remove unnecessary file

--- frontend/src/components/FormInput.js
+++ frontend/src/components/FormInput.js
@@ -1,9 +1,25 @@
+import { useState } from "react";
+import "./formInput.css";
+
 const FormInput = (props) => {
-  const { label, onChange, id, ...inputProps } = props;
+  const [focused, setFocused] = useState(false);
+  const { label, errorMessage, onChange, id, ...inputProps } = props;
+
+  const handleFocus = (e) => {
+    setFocused(true);
+  };
+
   return (
     <div className="formInput">
-      <label>{label}: </label>
-      <input {...inputProps} onChange={onChange} />
+      <label>{label}</label>
+      <input
+        {...inputProps}
+        onChange={onChange}
+        onBlur={handleFocus}
+        onFocus={() => inputProps.name === "password2" && setFocused(true)}
+        focused={focused.toString()}
+      />
+      <span>{errorMessage}</span>
     </div>
   );
 };
